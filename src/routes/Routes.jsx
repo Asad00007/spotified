b063e1ad247e2c0c28b9a871afe871fc8af6ce58
Asad00007 <asadsalim752@gmail.com>
2024-05-13 20:19:18 +0500

    Asad Commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "../pages/SignIn/SignIn";
 import OTP from "../pages/OTP/OTP";
 import Dashboard from "../pages/Dashboard/Dashboard";
@@ -8,44 +8,11 @@ import ManageOrganizer from "../pages/ManageOrganizers/ManageOrganizer";
 import OrganizerRequest from "../pages/OrganizerRequest/OrganizerRequest";
 import Settings from "../pages/Settings/Settings";
 import Profile from "../pages/Profile/Profile";
-const Routes = () => {
-  const Layout = () => {
-    return (
-      <>
-        <Sidebar />
-        <Navbar />
-        <Outlet />
-      </>
-    );
-  };
-  const router = createBrowserRouter([
-    {
-      path: "/signin",
-      element: <SignIn />,
-    },
-    {
-      path: "/otp",
-      element: <OTP />,
-    },
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/dashboard",
-          element: <Dashboard />,
-        },
-        { path: "/profile", element: <Settings /> },
-      ],
-    },
-    {
-      path: "/profile",
-      element: <Profile />,
-    },
-  ]);
+
+const AppRoutes = () => {
   return (
     <Router>
-      <Routing>
+      <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/otp" element={<OTP />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -54,9 +21,9 @@ const Routes = () => {
         <Route path="/organizer-request" element={<OrganizerRequest />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/profile" element={<Profile />} />
-      </Routing>
+      </Routes>
     </Router>
   );
 };
 
-export default Routes;
+export default AppRoutes;
