50f4b3981d1ae158937d00fbe15aa7efe180e7c1
Asad00007 <asadsalim752@gmail.com>
2024-05-30 16:31:25 +0500

    Fixed UI

--- src/pages/ManageOrganizers/ManageOrganizer.jsx
+++ src/pages/ManageOrganizers/ManageOrganizer.jsx
@@ -5,7 +5,9 @@ import filterIcon from "../../assets/filterIcon.svg";
 import lineFilter from "../../assets/lineFilter.svg";
 import downArrow from "../../assets/downArrow.svg";
 import deleteIcon from "../../assets/deleteIcon.svg";
+import { RotatingLines } from "react-loader-spinner";
 import { baseAxios } from "../../utils/apiConfig";
+import Popup from "../../components/Popup";
 import Cancel from "../../assets/icons/Cancel.svg";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
@@ -13,8 +15,10 @@ const ManageOrganizer = () => {
   const text = "Manage Organizer";
 
   const [organizers, setOrganizers] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalUsers, setTotalUsers] = useState(0);
+  const [current, setCurrent] = useState(0);
   const [perPage, setPerPage] = useState(10);
   const [blocked, setBlocked] = useState(false);
 
@@ -34,6 +38,11 @@ const ManageOrganizer = () => {
     } catch (error) {
       console.log("Error Deleting Organizer", error);
     }
+  };
+
+  const deleteOrganizer = (id) => {
+    handleDelete(id);
+    setCurrent(0);
   };
   const handleBlock = async (id) => {
     const accesstoken = sessionStorage.getItem("access_token");
@@ -89,9 +98,13 @@ const ManageOrganizer = () => {
     setIsUserDropdownOpen(!isUserDropdownOpen);
   };
 
+  const onCancel = () => {
+    setCurrent(0);
+  };
   const fetchOrganizers = async (offset = 0) => {
     const accesstoken = sessionStorage.getItem("access_token");
     try {
+      setLoading(true);
       const response = await baseAxios.get(
         `/admin_side/get_users/?role=organiser&limit=${perPage}&offset=${offset}`,
         {
@@ -101,7 +114,9 @@ const ManageOrganizer = () => {
       const data = response.data;
       setOrganizers(data.results);
       setTotalUsers(data.count);
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.log("Error Fetching Organizers", error);
     }
   };
@@ -232,76 +247,108 @@ const ManageOrganizer = () => {
                 </th>
               </tr>
             </thead>
-            <tbody>
-              {organizers.map((org) => (
-                <tr className="bg-white border-b text-[#202224]">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium whitespace-nowrap"
-                    key={org.id}
-                  >
-                    <input className="mr-2 accent-[#E0E0E0]" type="checkbox" />#
-                    {org.id}
-                  </th>
-                  <td className="px-6 py-4">{org.full_name}</td>
-                  <td className="px-6 py-4">{org.email}</td>
-                  <td className="px-6 py-4">{org.phoneNo}</td>
-                  <td className="px-6 py-4">{org.address}</td>
-                  <td className="px-6 py-4 flex gap-4">
-                    <svg
-                      width="30"
-                      height="30"
-                      viewBox="0 0 30 30"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      onClick={() => handleDelete(org.id)}
+            {!loading && (
+              <tbody>
+                {organizers.map((org) => (
+                  <tr className="bg-white border-b text-[#202224]">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium whitespace-nowrap"
+                      key={org.id}
                     >
-                      <rect
-                        x="0.5"
-                        y="0.5"
-                        width="29"
-                        height="29"
-                        rx="4.5"
-                        stroke="#393939"
-                        strokeOpacity="0.5"
+                      <input
+                        className="mr-2 accent-[#E0E0E0]"
+                        type="checkbox"
                       />
-                      <path
-                        d="M11.8887 10.2219L11.8887 8.92181C11.8887 8.01586 11.8887 7.56289 12.1604 7.28144C12.4322 7 12.8696 7 13.7444 7L16.2551 7C17.13 7 17.5674 7 17.8391 7.28144C18.1109 7.56289 18.1109 8.01586 18.1109 8.92181V10.2219"
-                        stroke="#393939"
-                        strokeOpacity="0.5"
-                      />
-                      <path
-                        d="M20.0318 17.549C19.8836 18.8906 19.8162 19.4559 19.6295 19.9033C19.2228 20.878 18.3978 21.6169 17.3844 21.9141C16.9191 22.0506 16.3499 22.0556 15 22.0556C13.6502 22.0556 13.081 22.0506 12.6157 21.9141C11.6023 21.6169 10.7773 20.878 10.3706 19.9033C10.1839 19.4559 10.1165 18.8906 9.96827 17.5489L9.15308 10.1681L20.847 10.1681L20.0318 17.549Z"
-                        stroke="#393939"
-                        strokeOpacity="0.5"
-                      />
-                      <path
-                        d="M8 10.206H22"
-                        stroke="#393939"
-                        strokeOpacity="0.5"
-                        strokeLinecap="round"
-                      />
-                    </svg>
-                    {org.approved ? (
-                      <button
-                        className=" text-white px-[5px] py-[5px] rounded ml-2 border border-gray-400 "
-                        onClick={() => handleBlock(org.id)}
+                      #{org.id}
+                    </th>
+                    <td className="px-6 py-4">{org.full_name}</td>
+                    <td className="px-6 py-4">{org.email}</td>
+                    <td className="px-6 py-4">{org.phoneNo}</td>
+                    <td className="px-6 py-4">{org.address}</td>
+                    <td className="px-6 py-4 flex gap-4">
+                      <svg
+                        width="30"
+                        height="30"
+                        viewBox="0 0 30 30"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="cursor-pointer"
+                        onClick={() => setCurrent(1)}
                       >
-                        <img src={Cancel} alt="" />
-                      </button>
-                    ) : (
-                      <button
-                        className=" text-white px-[5px] py-[5px] rounded ml-2 border border-gray-400 bg-gray-200"
-                        onClick={() => handleunBlock(org.id)}
-                      >
-                        <img src={Cancel} alt="" />
-                      </button>
-                    )}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+                        <rect
+                          x="0.5"
+                          y="0.5"
+                          width="29"
+                          height="29"
+                          rx="4.5"
+                          stroke="#393939"
+                          strokeOpacity="0.5"
+                        />
+                        <path
+                          d="M11.8887 10.2219L11.8887 8.92181C11.8887 8.01586 11.8887 7.56289 12.1604 7.28144C12.4322 7 12.8696 7 13.7444 7L16.2551 7C17.13 7 17.5674 7 17.8391 7.28144C18.1109 7.56289 18.1109 8.01586 18.1109 8.92181V10.2219"
+                          stroke="#393939"
+                          strokeOpacity="0.5"
+                        />
+                        <path
+                          d="M20.0318 17.549C19.8836 18.8906 19.8162 19.4559 19.6295 19.9033C19.2228 20.878 18.3978 21.6169 17.3844 21.9141C16.9191 22.0506 16.3499 22.0556 15 22.0556C13.6502 22.0556 13.081 22.0506 12.6157 21.9141C11.6023 21.6169 10.7773 20.878 10.3706 19.9033C10.1839 19.4559 10.1165 18.8906 9.96827 17.5489L9.15308 10.1681L20.847 10.1681L20.0318 17.549Z"
+                          stroke="#393939"
+                          strokeOpacity="0.5"
+                        />
+                        <path
+                          d="M8 10.206H22"
+                          stroke="#393939"
+                          strokeOpacity="0.5"
+                          strokeLinecap="round"
+                        />
+                      </svg>
+                      {current === 1 && (
+                        <Popup setTrigger={setCurrent}>
+                          <h1 className="text-xl font-bold mt-5 mb-3">
+                            Are you sure you want to delete this organiser?
+                          </h1>
+                          <div className="flex gap-5 mb-5">
+                            <button
+                              className="border-2 border-primary p-[3px] text-[13px] md:text-base w-[90px] md:w-[100px] rounded-md"
+                              onClick={onCancel}
+                            >
+                              No
+                            </button>
+                            <button
+                              className="bg-primary p-[3px] text-[13px] md:text-base w-[90px] md:w-[100px] rounded-md text-white"
+                              onClick={() => deleteOrganizer(org.id)}
+                            >
+                              Yes
+                            </button>
+                          </div>
+                        </Popup>
+                      )}
+                      {org.approved ? (
+                        <button
+                          className=" text-white px-[5px] py-[5px] rounded ml-2 border border-gray-400  cursor-pointer"
+                          onClick={() => handleBlock(org.id)}
+                        >
+                          <img src={Cancel} alt="" />
+                        </button>
+                      ) : (
+                        <button
+                          className=" text-white px-[5px] py-[5px] rounded ml-2 border border-gray-400 bg-gray-200 cursor-pointer"
+                          onClick={() => handleunBlock(org.id)}
+                        >
+                          <img src={Cancel} alt="" />
+                        </button>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            )}
           </table>
+          {loading && (
+            <div className="flex items-center justify-center mt-5">
+              <RotatingLines color="#123abc" loading={loading} width="40" />
+            </div>
+          )}
           <div className="flex justify-between items-center flex-1">
             <span className="text-[#202224] text-[14px] font-normal">
               Showing {currentPage * 10 - 9}-
